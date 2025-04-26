39a3d9eff044a29cc245677246bfdb935af75135
Glen Conway <glen.conway@kinsac.com>
2025-04-26 16:32:47 +0100

    Working SSE MCP server

--- InsightMCP/Program.cs
+++ InsightMCP/Program.cs
@@ -1,27 +1,28 @@
-﻿using InsightMCP;
-using InsightMCP.Services;
+﻿using InsightMCP.Services;
 using InsightMCP.Tools;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Logging.Console;
-using ModelContextProtocol.AspNetCore;
-using ModelContextProtocol.Server;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Configure logging for MCP debugging
-builder.Logging.SetMinimumLevel(LogLevel.Information);
+builder.Logging.SetMinimumLevel(LogLevel.Warning);
+builder.Logging.AddDebug();
 builder.Logging.AddFilter("ModelContextProtocol", LogLevel.Debug);
 builder.Logging.AddConsole();
 
 builder.Services
     .AddMcpServer()
-    .WithTools<ReportTools>();
+    .WithHttpTransport()
+    .WithTools<ReportTools>()
+    .WithTools<TestTools>();
 
 builder.Services.AddHttpClient();
-builder.Services.AddSingleton<ReportService>();
+builder.Services.AddScoped<IReportService, ReportService>();
 
 var app = builder.Build();
+
+app.MapMcp();
 
 app.Run();
